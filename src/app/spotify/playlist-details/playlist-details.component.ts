2a65741f847cf1dc633df721323e0d4e9fc1f83c
HarshPatel89 <harsh.b.patel@convergesolution.com>
2024-08-28 09:48:03 +0530

    added chart in playlist details

--- src/app/spotify/playlist-details/playlist-details.component.ts
+++ src/app/spotify/playlist-details/playlist-details.component.ts
@@ -9,16 +9,22 @@ import { TableModule } from 'primeng/table';
 import { ProgressBarComponent } from '../../shared/progress-bar/progress-bar.component';
 import { Router } from '@angular/router';
 import { HttpHeaders } from '@angular/common/http';
+import { ChartModule } from 'primeng/chart';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-playlist-details',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     ProgressBarComponent,
     MessagesModule,
     TableModule,
     ButtonModule,
+    ChartModule,
+    InputSwitchModule
   ],
   templateUrl: './playlist-details.component.html',
   styleUrl: './playlist-details.component.css'
@@ -31,6 +37,11 @@ export class PlaylistDetailsComponent implements OnInit {
   showAudioFeatures: boolean = false;
   checkInterval: any;
   isLoading: boolean = false;
+  data: any;
+  options: any;
+  showGraph: boolean = false;
+
+
 
   constructor(
     private spotifyService: SpotifyService,
@@ -42,7 +53,76 @@ export class PlaylistDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
+    this.data = {
+      labels: ['0:00:00'],
+      datasets: [
+        {
+          label: 'Tempo',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          tension: 0.4
+        },
+        {
+          label: 'Mode',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--pink-500'),
+          tension: 0.4
+        },
+        {
+          label: 'Key',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--teal-500'),
+          tension: 0.4
+        },
+        {
+          label: 'Loudness',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--orange-500'),
+          tension: 0.4
+        }
+      ]
+    };
+
+    this.options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.6,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false
+          }
+        }
+      }
+    };
   }
 
   ngOnDestroy(): void {
@@ -92,21 +172,55 @@ export class PlaylistDetailsComponent implements OnInit {
       this.isLoading = true;
       this.spotifyService.SpotifyCommonGetApi(url, spotifyAccessToken).subscribe((resp) => {
         this.playlistTracks = resp.items;
+
+
         // To assign Audio Features to the track
-        this.playlistTracks.forEach(pltrack => {
+        this.playlistTracks.forEach((pltrack) => {
           this.spotifyService.getSpotifyAudioFeaturesUrl(pltrack.track.id).subscribe((res) => {
             if (res.statusCode === 200) {
               var featuresUrl = res.payload;
               const spotifyAccessToken = sessionStorage.getItem('spotify-bearer-token') || '';
               this.spotifyService.SpotifyCommonGetApi(featuresUrl, spotifyAccessToken).subscribe((res) => {
                 pltrack.audioFeatures = res;
-              });
 
+                this.spotifyService.getSpotifyAudioAnalysisUrl(pltrack.track.id).subscribe((res) => {
+                  if (res.statusCode === 200) {
+                    var analysisUrl = res.payload;
+                    const spotifyAccessToken = sessionStorage.getItem('spotify-bearer-token') || '';
+                    this.spotifyService.SpotifyCommonGetApi(analysisUrl, spotifyAccessToken).subscribe((res) => {
+                      pltrack.audioAnalysis = res;
+                    });
+                  }
+                });
+
+              });
             }
           });
         });
-        this.isLoading = false;
-        console.log('original',this.playlistTracks);
+
+        console.log('original', this.playlistTracks);
+        var durationSum = 0;
+        setTimeout(() => {
+          this.playlistTracks.forEach(pltrack => {
+            pltrack.audioAnalysis.sections.forEach((section: any) => {
+              durationSum = durationSum + ((section.duration) * 1000);
+              //duration
+              this.data.labels.push(`${pltrack?.track.name} ${Constants.formatMilliseconds(durationSum)}`);
+              //tempo
+              this.data.datasets[0].data.push(section.tempo);
+              //mode
+              this.data.datasets[1].data.push(section.mode);
+              //key
+              this.data.datasets[2].data.push(section.key);
+              //loudness
+              this.data.datasets[3].data.push(section.loudness);
+            });
+          });
+          this.isLoading = false;
+          this.showGraph = true;
+        }, 2000);
+
+        console.log('data', this.data);
       });
     };
   }
@@ -139,6 +253,66 @@ export class PlaylistDetailsComponent implements OnInit {
   }
 
   updatePlaylist() {
-    console.log('Reordered',this.playlistTracks);
+    console.log('Reordered', this.playlistTracks);
+  }
+
+  tableReordered(){
+    this.showGraph = false;
+    const documentStyle = getComputedStyle(document.documentElement);
+
+    this.data = {
+      labels: ['0:00:00'],
+      datasets: [
+        {
+          label: 'Tempo',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          tension: 0.4
+        },
+        {
+          label: 'Mode',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--pink-500'),
+          tension: 0.4
+        },
+        {
+          label: 'Key',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--teal-500'),
+          tension: 0.4
+        },
+        {
+          label: 'Loudness',
+          data: [0],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--orange-500'),
+          tension: 0.4
+        }
+      ]
+    };
+    console.log('Reordered', this.playlistTracks);
+    
+    var durationSum = 0;
+        setTimeout(() => {
+          this.playlistTracks.forEach(pltrack => {
+            pltrack.audioAnalysis.sections.forEach((section: any) => {
+              durationSum = durationSum + ((section.duration) * 1000);
+              //duration
+              this.data.labels.push(`${pltrack?.track.name} ${Constants.formatMilliseconds(durationSum)}`);
+              //tempo
+              this.data.datasets[0].data.push(section.tempo);
+              //mode
+              this.data.datasets[1].data.push(section.mode);
+              //key
+              this.data.datasets[2].data.push(section.key);
+              //loudness
+              this.data.datasets[3].data.push(section.loudness);
+            });
+          });
+          this.showGraph = true;
+        }, 2000);
   }
 }
