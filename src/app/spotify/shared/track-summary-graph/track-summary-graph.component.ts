2e1c211422d2fa3e739db4cff8ca50039d067195
HarshPatel89 <harsh.b.patel@convergesolution.com>
2024-09-26 09:51:13 +0530

    added shared component for track summary graph

---
+++ src/app/spotify/shared/track-summary-graph/track-summary-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-summary-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './track-summary-graph.component.html',
+  styleUrl: './track-summary-graph.component.css'
+})
+export class TrackSummaryGraphComponent {
+
+}
