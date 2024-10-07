971496fb82258d5b1264e630908cfcb6c2db8c16
HarshPatel89 <harsh.b.patel@convergesolution.com>
2024-10-07 15:16:56 +0530

    added new component

---
+++ src/app/spotify/shared/projections-graph/projections-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projections-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './projections-graph.component.html',
+  styleUrl: './projections-graph.component.css'
+})
+export class ProjectionsGraphComponent {
+
+}
